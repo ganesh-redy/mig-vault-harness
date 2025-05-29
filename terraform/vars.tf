11e4544e6654a16b4409ec9c4cdaf325c11842ba
ganesh-redy <ganeshreddy7610@gmail.com>
2025-05-29 15:17:13 +0530

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "The GCP project ID"
+  type        = string
+  
+}
+
+variable "zone" {
+  description = "The region where the resources will be created"
+  type        = string
+    
+}
